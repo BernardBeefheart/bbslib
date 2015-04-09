db661587e132319d696ec4e58ed65daf68b6cae5
bernard <bernard.beefheart@outlook.fr>
2015-04-10 00:45:29 +0200

    make-chicken-units : une bonne gestion des erreurs sur les fichiers

--- make-chicken-units/make-chicken-units.scm
+++ make-chicken-units/make-chicken-units.scm
@@ -7,22 +7,36 @@
 (require-extension matchable)
 (declare (uses extras))
 
+(define *exe-name* "make-chicken-units")
+(define *version* "1.0.0")
+
 (define dohelp
   (lambda (exit-code)
-	(printf "make-chicken-units [--help] : this text~%")
-	(printf "make-chicken-units source-file unit output-file~%")
+	(printf "~A version ~A~%" *exe-name* *version*)
+	(printf "~A [--help] : this text~%" *exe-name*)
+	(printf "~A source-file unit output-file~%" *exe-name*)
 	(exit exit-code)))
 
+(define on-file-error 
+  (lambda (direction exn)
+	(printf "ERROR (~A): ~A ~A~%" 
+			direction 
+			((condition-property-accessor 'exn 'message) exn)
+			((condition-property-accessor 'exn 'arguments) exn))
+	(dohelp 1)))
 
-(define (check thunk)
-  (condition-case (thunk)
-    [(exn file) (printf "File error~%") (dohelp 1)]
-    [(exn) (printf "Other error~%") (dohelp 1)]
-    [var () (print "something else") (dohelp 1)]))
+(define handle-file-operation
+  (lambda (direction chunk)
+	(handle-exceptions exn
+					   (on-file-error direction exn)
+					   (chunk))))
 
 (define get-file
   (lambda (file-name)
-	(check (lambda () (read-lines file-name)))))
+	(handle-file-operation 
+	  "read"
+	  (lambda ()
+		(read-lines file-name)))))
 
 (define file->unit
   (lambda (file-name unit-name output-file-name)
@@ -32,18 +46,21 @@
 				   (when (not (null? rest))
 					 (fprintf port "~A~%" (car rest))
 					 (write-file-content port (cdr rest))))))
-		(call-with-output-file output-file-name
-							   (lambda (port)
-								 (fprintf port "(declare (unit ~A))~%" unit-name)
-								 (write-file-content port file-content)))))))
+		(handle-file-operation
+		  "write"
+		  (lambda ()
+			(call-with-output-file output-file-name
+								   (lambda (port)
+									 (fprintf port "(declare (unit ~A))~%" unit-name)
+									 (write-file-content port file-content)))))))))
 
 (define main
   (lambda()
 	(let ((args (cdr (argv))))
 	  (match args
-			 (("--help") (dohelp 0))
 			 (() (dohelp 0))
+			 (("--help") (dohelp 0))
 			 ((source-file unit output-file) (file->unit source-file unit output-file))
 			 (_ (dohelp 1))))))
-			 
+
 (main)
