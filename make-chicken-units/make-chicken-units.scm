7440a078787e6a1b4399fea7cae0eee7d148be42
bernard <bernard.beefheart@outlook.fr>
2015-04-09 16:29:12 +0200

    gestion propre (ou presque) lors de l'erreur de lecture du fichier d'entrée

--- make-chicken-units/make-chicken-units.scm
+++ make-chicken-units/make-chicken-units.scm
@@ -7,9 +7,22 @@
 (require-extension matchable)
 (declare (uses extras))
 
+(define dohelp
+  (lambda (exit-code)
+	(printf "make-chicken-units [--help] : this text~%")
+	(printf "make-chicken-units source-file unit output-file~%")
+	(exit exit-code)))
+
+
+(define (check thunk)
+  (condition-case (thunk)
+    [(exn file) (printf "File error~%") (dohelp 1)]
+    [(exn) (printf "Other error~%") (dohelp 1)]
+    [var () (print "something else") (dohelp 1)]))
+
 (define get-file
   (lambda (file-name)
-	(read-lines file-name)))
+	(check (lambda () (read-lines file-name)))))
 
 (define file->unit
   (lambda (file-name unit-name output-file-name)
@@ -23,13 +36,6 @@
 							   (lambda (port)
 								 (fprintf port "(declare (unit ~A))~%" unit-name)
 								 (write-file-content port file-content)))))))
-
-(define dohelp
-  (lambda (exit-code)
-	(printf "make-chicken-units [--help] : this text~%")
-	(printf "make-chicken-units source-file unit output-file~%")
-	(exit exit-code)))
-
 
 (define main
   (lambda()
