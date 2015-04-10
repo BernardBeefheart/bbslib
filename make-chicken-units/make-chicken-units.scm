2b5dbe34f2b6ea81c61eeae5401e171096b34b3f
bernard <bernard.beefheart@outlook.fr>
2015-04-10 21:44:17 +0200

    make-chicken-units : meilleure gestion des erreurs, l'application est une classe

--- make-chicken-units/make-chicken-units.scm
+++ make-chicken-units/make-chicken-units.scm
@@ -5,17 +5,38 @@
 ;; ======================================================================
 
 (require-extension matchable)
+(require-extension coops)
+(require-extension coops-primitive-objects)
+
 (declare (uses extras))
 
+(define-class <Application> ()
+			  ((exe-name initform: "no-name-app")
+			   (version initform: "1.0.0")))
+
+(define-generic (_dohelp (self <Application>) (exit-code <integer>)))
+
+(define-method (_dohelp (self <Application>) (exit-code <integer>))
+	(printf "~A version ~A~%" (slot-value self 'exe-name) (slot-value self 'version))
+	(printf "~A [--help] : this text~%" (slot-value self 'exe-name))
+	(printf "~A source-file unit output-file~%" (slot-value self 'exe-name))
+	(exit exit-code))
+
+#|
 (define *exe-name* "make-chicken-units")
 (define *version* "1.0.0")
+|#
+
+(define *application* (make <Application> 'exe-name "make-chicken-units" 'version "1.0.0"))
 
 (define dohelp
   (lambda (exit-code)
-	(printf "~A version ~A~%" *exe-name* *version*)
-	(printf "~A [--help] : this text~%" *exe-name*)
-	(printf "~A source-file unit output-file~%" *exe-name*)
-	(exit exit-code)))
+	(_dohelp *application* exit-code)))
+
+(define on-bad-argument
+  (lambda()
+	(printf "ERROR : bad command line arguments~%")
+	(dohelp 1)))
 
 (define on-file-error 
   (lambda (direction exn)
@@ -61,6 +82,6 @@
 			 (() (dohelp 0))
 			 (("--help") (dohelp 0))
 			 ((source-file unit output-file) (file->unit source-file unit output-file))
-			 (_ (dohelp 1))))))
+			 (_ (on-bad-argument))))))
 
 (main)
