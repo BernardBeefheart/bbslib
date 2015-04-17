ff17004da0f3d864514683ff6d6ac197e82210fa
bernard <bernard.beefheart@outlook.fr>
2015-04-17 22:03:06 +0200

    le chargement de la table des matières fonctionne, ajout chargement du pied de page

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,5 +1,6 @@
 $(document).ready(function () {
 	$('#main_content').headsmart();
-	$( "#toc" ).load( "http://bernardbeefheart.github.io/bbslib/content.html");
+	$( '#toc' ).load( 'http://bernardbeefheart.github.io/bbslib/content.html');
+	$( 'footer' ).load( 'http://bernardbeefheart.github.io/bbslib/footer.html');
 })
 
