67164dc60066a19b3acd9c741b53bb68f1d92d2f
bernard <bernard.beefheart@outlook.fr>
2015-04-18 23:08:51 +0200

    match: test.scm, utilisation de tester

--- match/test.scm
+++ match/test.scm
@@ -2,32 +2,30 @@
 ;; test.scm
 ;;
 ;; ======================================================================
-(import (scheme base) (println) (match-case-simple))
+(import (scheme base) (println) (match-case-simple) (tester))
 
 
 (define test-match 
   (lambda (x)
 	(match-case-simple x
-					   (,x (integer? x) "integer")
-					   (,x (string? x) "string")
-					   (,x (symbol? x) "symbol")
-					   (() ()          "nil")
-					   (#t ()          "bool")
-					   (#f ()          "bool")
+					   (,x (integer? x) 'integer)
+					   (,x (string? x) 'string)
+					   (,x (symbol? x) 'symbol)
+					   (() ()          'nil)
+					   (#t ()          'bool)
+					   (#f ()          'bool)
 					   ((,x . ,y) ()     
-								  (string-append "pair of " (test-match x) " and " (test-match y)))
-					   (__ ()           "something else"))))
+								  (list 'pair-of (test-match x) 'and (test-match y)))
+					   (__ ()           'something-else))))
 
-;; printed result:
-
-;; integer
-;; bool
-;; symbol
-;; string
-;; something else
-;; pair of integer and pair of integer and pair of integer and nil
-(for-each (lambda (x) (println (test-match x)))
-		  '(1 #t 'symbol "str" 1.235 (1 2 3)))
+(test-begin "match-case-simple")
+(test-equal "test-match" 
+            (map test-match '(1 #t 'symbol "str" 1.235 (1 2 3)))
+            '(integer bool 
+              (pair-of symbol and (pair-of symbol and nil)) 
+              string something-else 
+              (pair-of integer and (pair-of integer and (pair-of integer and nil)))))
+(test-end "match-case-simple")
 
 
 
