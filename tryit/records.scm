3ec93e4256f6d0e494f78c5e470989f9244a841a
bernard <bernard.beefheart@outlook.fr>
2015-04-21 00:25:55 +0200

    tryit: records.scm pour tester les structures

---
+++ tryit/records.scm
@@ -0,0 +1,26 @@
+;; ======================================================================
+;; records.scm
+;; ======================================================================
+
+(import (scheme base) (scheme inexact) (println))
+
+(define-record-type <point>
+  (make-point x y)           ;; constructeur
+  point?                     ;; prédicat
+  (x point-x point-x!)       ;; champs x, accesseur, modificateur
+  (y point-y point-y!))      ;; champ y ...
+
+(define distance 
+  (lambda (p1 p2)
+    (when (not (point? p1))
+      (raise "distance: p1 must be a point"))
+    (when (not (point? p2))
+      (raise "distance: p2 must be a point"))
+    (let ((dx (- (point-x p2) (point-x p1)))
+          (dy (- (point-y p2) (point-y p1))))
+      (sqrt (inexact (+ (* dx dx) (* dy dy)))))))
+
+(define origine (make-point 0 0))
+(define p1 (make-point 1 1))
+
+(println "distance p1 origine: " (distance p1 origine))
