0eb400630c798d89654a27d88bdb5a0633b8538d
bernard <bernard.beefheart@outlook.fr>
2015-04-21 00:26:24 +0200

    tools: la gestion des exceptions

---
+++ tools/exception.scm
@@ -0,0 +1,34 @@
+;; ======================================================================
+;; execption.scm
+;; ======================================================================
+
+(define-library
+ (exception)
+ (import (scheme base) (println))
+ (export raise-exception print-exception)
+ (begin
+   
+   (define-record-type <exception>
+     (make-exception type     ;; error, fatal error, warning...
+                     source   ;; file, function
+                     text)
+     exception?
+     (type exception-type)
+     (source exception-source)
+     (text exception-text))
+   
+   (define raise-exception
+     (lambda (type source text)
+       (raise (make-exception type source text))))
+   
+   (define print-exception
+     (lambda (exception)
+       (cond
+        ((string? exception) (println "ERROR: " exception))
+        ((exception? exception) (println (exception-type exception) ": " 
+                                         (exception-source exception) " - " 
+                                         (exception-text exception)))
+        (else (println "ERROR of unknown type: " exception)))))
+   
+   ))
+ 
