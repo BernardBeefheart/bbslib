454271aa00174d721894b47a8ce16ec8785a08f3
bernard <bernard.beefheart@outlook.fr>
2015-04-22 00:19:48 +0200

    remise en forme du code

--- tinyclos-for-r7rs/support.scm
+++ tinyclos-for-r7rs/support.scm
@@ -42,219 +42,213 @@
 ;
 
 (define-library 
- (support)
- (import (scheme base))
-(cond-expand
- ((or foment sagittarius) (import (sort)))
- ((or chibi gosh) (import (srfi 95)))
- (else '()))
- (export gsort simple-printer ??? list* apply* 
-         position-of map-append last every remove getl union
-         collect-if remove-duplicates compute-std-cpl
-         top-sort std-tie-breaker build-transitive-closure build-constraints)
- (begin
-
-
-(define gsort
+  (support)
+  (import (scheme base))
   (cond-expand
-    ((or foment sagittarius) (lambda(predicate list) (merge-sort predicate list)))
-    ((or chibi gosh)     (lambda(predicate list) (sort list predicate)))
-    (else     '())))
+	((or foment sagittarius) (import (srfi-95-sort)))
+	((or chibi gosh) (import (srfi 95)))
+	(else '()))
+  (export gsort simple-printer ??? list* apply* 
+		  position-of map-append last every remove getl union
+		  collect-if remove-duplicates compute-std-cpl
+		  top-sort std-tie-breaker build-transitive-closure build-constraints)
+  (begin
+
+
+	(define gsort
+	  (cond-expand
+		((or foment sagittarius) (lambda(predicate list) (merge-sort predicate list)))
+		((or chibi gosh)     (lambda(predicate list) (sort list predicate)))
+		(else     '())))
+
+	(define ??? 'unspecified-result)
+	(define simple-printer (lambda () ???))
+
+	(define list*
+	  (lambda args
+		(letrec ((chase
+				   (lambda (args)
+					 (cond ((null? args) '())
+						   ((null? (cdr args)) (car args))
+						   (else (cons (car args) (chase (cdr args))))))))
+		  (chase args))))
+
+	(define apply*
+	  (lambda (proc . args)
+		(apply proc (apply list* args))))
+
+
+	(define position-of
+	  (lambda (x lst)
+		(if (eq? x (car lst)) 0 (+ 1 (position-of x (cdr lst))))))
+
+	(define map-append
+	  (lambda (proc . lists)
+		(apply append (apply map (cons proc lists)))))
+
+	(define last
+	  (lambda (l)
+		(if (null? l)
+		  #f
+		  (if (null? (cdr l))
+			(car l)
+			(last (cdr l))))))
+
+	(define every
+	  (lambda (test . lists)
+		(let scan ((tails lists))
+		  (if (member #t (map null? tails))             ;(any null? lists)
+			#t
+			(and (apply test (map car tails))
+				 (scan (map cdr tails)))))))
+
+	(define remove
+	  (lambda (x list)
+		(cond ((null? list) '())
+			  ((eq? (car list) x) (cdr list))
+			  (else (cons (car list) (remove x (cdr list)))))))
+
+	(define getl
+	  (lambda (initargs name . not-found)
+		(letrec ((scan (lambda (tail)
+						 (cond ((null? tail)
+								(if (pair? not-found)
+								  (car not-found)
+								  (error "GETL couldn't find" name)))
+							   ((eq? (car tail) name) (cadr tail))
+							   (else (scan (cddr tail)))))))
+		  (scan initargs))))
+
+	(define union
+	  (lambda lists
+		(letrec ((clean (lambda (list result)
+						  (cond ((null? list) result)
+								((memq (car list) result)
+								 (clean (cdr list) result))
+								(else
+								  (clean (cdr list) (cons (car list) result)))))))
+		  (clean (apply append lists) '()))))
+
+	(define collect-if
+	  (lambda (test? list)
+		(cond ((null? list) '())
+			  ((test? (car list)) (cons (car list) (collect-if test? (cdr list))))
+			  (else (collect-if test? (cdr list))))))
+
+	;(define remove-unless
+	;    (lambda (test list)
+	;      (if (null? list)
+	;	  ()
+	;	  (let ((rest (remove-unless test (cdr list))))
+	;	    (if (test (car list))
+	;		(cons (car list) rest)
+	;		rest)))))
+
+	(define remove-duplicates
+	  (lambda (list)
+		(let loop ((result-so-far '())
+				   (remaining list))
+		  (if (null? remaining)
+			result-so-far
+			(if (null? (memq (car remaining) result-so-far))
+			  (loop (cons (car remaining) result-so-far)
+					(cdr remaining))
+			  (loop result-so-far
+					(cdr remaining)))))))
 
 
 
 
+	;
+	; A simple topological sort.
+	;
+	; It's in this file so that both TinyClos and Objects can use it.
+	;
+	; This is a fairly modified version of code I originally got from Anurag
+	; Mendhekar <anurag@moose.cs.indiana.edu>.
+	;
+	;
+
+	(define compute-std-cpl
+	  (lambda (c get-direct-supers)
+		(top-sort ((build-transitive-closure get-direct-supers) c)
+				  ((build-constraints get-direct-supers) c)
+				  (std-tie-breaker get-direct-supers))))
 
 
+	(define top-sort
+	  (lambda (elements constraints tie-breaker)
+		(let loop ((elements    elements)
+				   (constraints constraints)
+				   (result      '()))
+		  (if (null? elements)
+			result
+			(let ((can-go-in-now
+					(collect-if
+					  (lambda (x)
+						(every (lambda (constraint)
+								 (or (not (eq? (cadr constraint) x))
+									 (memq (car constraint) result)))
+							   constraints))
+					  elements)))
+			  (if (null? can-go-in-now)
+				(error 'top-sort "Invalid constraints")
+				(let ((choice (if (null? (cdr can-go-in-now))
+								(car can-go-in-now)
+								(tie-breaker result
+											 can-go-in-now))))
+				  (loop
+					(collect-if (lambda (x) (not (eq? x choice)))
+								elements)
+					constraints
+					(append result (list choice))))))))))
 
-(define ??? 'unspecified-result)
-(define simple-printer (lambda () ???))
-
-(define list*
-  (lambda args
-    (letrec ((chase
-	      (lambda (args)
-		(cond ((null? args) '())
-		      ((null? (cdr args)) (car args))
-		      (else (cons (car args) (chase (cdr args))))))))
-      (chase args))))
-
-(define apply*
-    (lambda (proc . args)
-      (apply proc (apply list* args))))
-
-
-(define position-of
-    (lambda (x lst)
-      (if (eq? x (car lst)) 0 (+ 1 (position-of x (cdr lst))))))
-
-(define map-append
-    (lambda (proc . lists)
-      (apply append (apply map (cons proc lists)))))
-
-(define last
-    (lambda (l)
-      (if (null? l)
-	  #f
-	  (if (null? (cdr l))
-	      (car l)
-	      (last (cdr l))))))
-
-(define every
-    (lambda (test . lists)
-      (let scan ((tails lists))
-	(if (member #t (map null? tails))             ;(any null? lists)
-	    #t
-	    (and (apply test (map car tails))
-		 (scan (map cdr tails)))))))
-
-(define remove
-    (lambda (x list)
-      (cond ((null? list) '())
-	    ((eq? (car list) x) (cdr list))
-	    (else (cons (car list) (remove x (cdr list)))))))
-
-(define getl
-    (lambda (initargs name . not-found)
-      (letrec ((scan (lambda (tail)
-		       (cond ((null? tail)
-			      (if (pair? not-found)
-				  (car not-found)
-				  (error "GETL couldn't find" name)))
-			     ((eq? (car tail) name) (cadr tail))
-			     (else (scan (cddr tail)))))))
-	(scan initargs))))
-
-(define union
-    (lambda lists
-      (letrec ((clean (lambda (list result)
-			(cond ((null? list) result)
-			      ((memq (car list) result)
-			       (clean (cdr list) result))
-			      (else
-			       (clean (cdr list) (cons (car list) result)))))))
-	(clean (apply append lists) '()))))
-
-(define collect-if
-    (lambda (test? list)
-      (cond ((null? list) '())
-	    ((test? (car list)) (cons (car list) (collect-if test? (cdr list))))
-	    (else (collect-if test? (cdr list))))))
-
-;(define remove-unless
-;    (lambda (test list)
-;      (if (null? list)
-;	  ()
-;	  (let ((rest (remove-unless test (cdr list))))
-;	    (if (test (car list))
-;		(cons (car list) rest)
-;		rest)))))
-
-(define remove-duplicates
-    (lambda (list)
-      (let loop ((result-so-far '())
-		 (remaining list))
-	   (if (null? remaining)
-	       result-so-far
-	       (if (null? (memq (car remaining) result-so-far))
-		   (loop (cons (car remaining) result-so-far)
-			 (cdr remaining))
-		   (loop result-so-far
-			 (cdr remaining)))))))
+	(define std-tie-breaker
+	  (lambda (get-supers)
+		(lambda (partial-cpl min-elts)
+		  (let loop ((pcpl (reverse partial-cpl)))
+			(let ((current-elt (car pcpl)))
+			  (let ((ds-of-ce (get-supers current-elt)))
+				(let ((common (collect-if (lambda (x)
+											(memq x ds-of-ce))
+										  min-elts)))
+				  (if (null? common)
+					(if (null? (cdr pcpl))
+					  (error 'std-tie-breaker "Nothing valid")
+					  (loop (cdr pcpl)))
+					(car common)))))))))
 
 
+	(define build-transitive-closure
+	  (lambda (get-follow-ons)
+		(lambda (x)
+		  (let track ((result '())
+					  (pending (list x)))
+			(if (null? pending)
+			  result
+			  (let ((next (car pending)))
+				(if (memq next result)
+				  (track result (cdr pending))
+				  (track (cons next result)
+						 (append (get-follow-ons next)
+								 (cdr pending))))))))))
 
+	(define build-constraints
+	  (lambda (get-follow-ons)
+		(lambda (x)
+		  (let loop ((elements ((build-transitive-closure get-follow-ons) x))
+					 (this-one '())
+					 (result '()))
+			(if (or (null? this-one) (null? (cdr this-one)))
+			  (if (null? elements)
+				result
+				(loop (cdr elements)
+					  (cons (car elements)
+							(get-follow-ons (car elements)))
+					  result))
+			  (loop elements
+					(cdr this-one)
+					(cons (list (car this-one) (cadr this-one))
+						  result)))))))
 
-;
-; A simple topological sort.
-;
-; It's in this file so that both TinyClos and Objects can use it.
-;
-; This is a fairly modified version of code I originally got from Anurag
-; Mendhekar <anurag@moose.cs.indiana.edu>.
-;
-;
-
-(define compute-std-cpl
-    (lambda (c get-direct-supers)
-      (top-sort ((build-transitive-closure get-direct-supers) c)
-		((build-constraints get-direct-supers) c)
-		(std-tie-breaker get-direct-supers))))
-
-
-(define top-sort
-    (lambda (elements constraints tie-breaker)
-      (let loop ((elements    elements)
-		 (constraints constraints)
-		 (result      '()))
-	(if (null? elements)
-	    result
-	    (let ((can-go-in-now
-		    (collect-if
-		      (lambda (x)
-			(every (lambda (constraint)
-				 (or (not (eq? (cadr constraint) x))
-				     (memq (car constraint) result)))
-			       constraints))
-		      elements)))
-	      (if (null? can-go-in-now)
-		  (error 'top-sort "Invalid constraints")
-		  (let ((choice (if (null? (cdr can-go-in-now))
-				    (car can-go-in-now)
-				    (tie-breaker result
-						 can-go-in-now))))
-		    (loop
-		      (collect-if (lambda (x) (not (eq? x choice)))
-			          elements)
-		      constraints
-		      (append result (list choice))))))))))
-
-(define std-tie-breaker
-    (lambda (get-supers)
-      (lambda (partial-cpl min-elts)
-	(let loop ((pcpl (reverse partial-cpl)))
-	     (let ((current-elt (car pcpl)))
-	       (let ((ds-of-ce (get-supers current-elt)))
-		 (let ((common (collect-if (lambda (x)
-					     (memq x ds-of-ce))
-					   min-elts)))
-		   (if (null? common)
-		       (if (null? (cdr pcpl))
-			   (error 'std-tie-breaker "Nothing valid")
-			   (loop (cdr pcpl)))
-		       (car common)))))))))
-
-
-(define build-transitive-closure
-    (lambda (get-follow-ons)
-      (lambda (x)
-	(let track ((result '())
-		    (pending (list x)))
-	     (if (null? pending)
-		 result
-		 (let ((next (car pending)))
-		   (if (memq next result)
-		       (track result (cdr pending))
-		       (track (cons next result)
-			      (append (get-follow-ons next)
-				      (cdr pending))))))))))
-
-(define build-constraints
-  (lambda (get-follow-ons)
-    (lambda (x)
-      (let loop ((elements ((build-transitive-closure get-follow-ons) x))
-		 (this-one '())
-		 (result '()))
-	   (if (or (null? this-one) (null? (cdr this-one)))
-	       (if (null? elements)
-		   result
-		   (loop (cdr elements)
-			 (cons (car elements)
-			       (get-follow-ons (car elements)))
-			 result))
-	       (loop elements
-		     (cdr this-one)
-		     (cons (list (car this-one) (cadr this-one))
-			   result)))))))
-
-))
+	))
