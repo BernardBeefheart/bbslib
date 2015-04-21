a81924f525358f62de6e52e0b44da5400ef1e2d1
bernard <bernard.beefheart@outlook.fr>
2015-04-21 02:01:44 +0200

    sl-printf: les valeurs par défauts sont des constantes fabriquées par une macro...

--- sl-printf/slprintf.scm
+++ sl-printf/slprintf.scm
@@ -10,29 +10,35 @@
   (begin
 
 	
+    ;; idée limite: faire une macro pour obtenir une constante...
+    (define-syntax default-filler
+      (syntax-rules ()
+        ((default-filler) #\space)))
 
+    (define-syntax default-len
+      (syntax-rules ()
+        ((default-len) -1)))
+    
 	(define (slprintf format . all-args)
-	  (let ((lformat (string->list format))
-			(default-filler #\space)
-			(default-len -1))
+	  (let ((lformat (string->list format)))
 		(letrec ((display-and-deformat
 				   (lambda(strs lst-of-chars args)
                      (display strs)
-					 (deformat (cdr lst-of-chars) args #f default-filler default-len)))
+					 (deformat (cdr lst-of-chars) args #f (default-filler) (default-len))))
 				 (on-else
 				   (lambda(c lst-of-chars args)
 					 (display c)
-					 (deformat (cdr lst-of-chars) args #f default-filler default-len)))
+					 (deformat (cdr lst-of-chars) args #f (default-filler) (default-len))))
 				 (deformat
 				   (lambda(lst-of-chars args in-format filler len)
 					 (if (not (null? lst-of-chars))
 					   (let ((c (car lst-of-chars)))
 						 (case c
 						   ((#\newline) (newline)
-										(deformat (cdr lst-of-chars) args #f default-filler default-len))
-						   ((#\%) (deformat (cdr lst-of-chars) args #t default-filler default-len))
+										(deformat (cdr lst-of-chars) args #f (default-filler) (default-len)))
+						   ((#\%) (deformat (cdr lst-of-chars) args #t (default-filler) (default-len)))
 						   ((#\space #\0) (if in-format
-											(deformat (cdr lst-of-chars) args #t #\0 default-len)
+											(deformat (cdr lst-of-chars) args #t c (default-len))
 											(on-else c lst-of-chars args)))
 						   ((#\1 #\2 #\3 #\4 #\5 #\6 #\7 #\8 #\9) (if in-format
 																	(deformat (cdr lst-of-chars) args #t filler (- (char->integer c) (char->integer #\0)))
@@ -57,6 +63,6 @@
                                 (raise-exception 'ERROR 'sl-printf "Bad format definition")
                                 (display-and-deformat c lst-of-chars args)))))
 					   #t))))
-		  (deformat lformat all-args #f default-filler default-len))))
+		  (deformat lformat all-args #f (default-filler) (default-len)))))
 
 	))
