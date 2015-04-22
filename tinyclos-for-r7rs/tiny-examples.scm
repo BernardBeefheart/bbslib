ad8c6e43f5a95be09ade51b11934dc8581bbbbf4
bernard <bernard.beefheart@outlook.fr>
2015-04-22 13:48:48 +0200

    tinyclos: indentation du code

--- tinyclos-for-r7rs/tiny-examples.scm
+++ tinyclos-for-r7rs/tiny-examples.scm
@@ -39,15 +39,15 @@
 ;
 ;
 (define initialize-slots
-    (lambda (object initargs)
-      (let ((not-there (list 'shes-not-there)))
-	(for-each (lambda (slot)
-		    (let ((name (car slot)))
-		      (let ((value  (getl initargs name not-there)))
-			(if (eq? value not-there)
-			    'do-nothing
-			    (slot-set! object name value)))))
-		  (class-slots (class-of object))))))
+  (lambda (object initargs)
+	(let ((not-there (list 'shes-not-there)))
+	  (for-each (lambda (slot)
+				  (let ((name (car slot)))
+					(let ((value  (getl initargs name not-there)))
+					  (if (eq? value not-there)
+						'do-nothing
+						(slot-set! object name value)))))
+				(class-slots (class-of object))))))
 
 
 
@@ -60,14 +60,14 @@
 ;
 
 (define <pos> (make <class>                          ;[make-class 
-		    'direct-supers (list <object>)   ;  (list <object>) 
-		    'direct-slots  (list 'x 'y)))    ;  (list 'x 'y)]
+					'direct-supers (list <object>)   ;  (list <object>) 
+					'direct-slots  (list 'x 'y)))    ;  (list 'x 'y)]
 
 (add-method initialize
-    (make-method (list <pos>)
-      (lambda (call-next-method pos initargs)
-	(call-next-method)
-	(initialize-slots pos initargs))))
+			(make-method (list <pos>)
+						 (lambda (call-next-method pos initargs)
+						   (call-next-method)
+						   (initialize-slots pos initargs))))
 
 (define p1 (make <pos> 'x 1 'y 2))
 (define p2 (make <pos> 'x 3 'y 5))
@@ -87,29 +87,29 @@
 (define move  (make-generic))
 
 (let ((x (vector 'x))
-      (y (vector 'y)))
-  
+	  (y (vector 'y)))
+
   (set! <pos> (make <class>
-		    'direct-supers (list <object>)
-		    'direct-slots  (list x y)))
+					'direct-supers (list <object>)
+					'direct-slots  (list x y)))
 
   (add-method pos-x
-      (make-method (list <pos>)
-	(lambda (call-next-method pos) (slot-ref pos x))))
+			  (make-method (list <pos>)
+						   (lambda (call-next-method pos) (slot-ref pos x))))
   (add-method pos-y
-      (make-method (list <pos>)
-	(lambda (call-next-method pos) (slot-ref pos y))))
+			  (make-method (list <pos>)
+						   (lambda (call-next-method pos) (slot-ref pos y))))
 
   (add-method move
-      (make-method (list <pos>)
-        (lambda (call-next-method pos new-x new-y)
-	  (slot-set! pos x new-x)
-	  (slot-set! pos y new-y))))
+			  (make-method (list <pos>)
+						   (lambda (call-next-method pos new-x new-y)
+							 (slot-set! pos x new-x)
+							 (slot-set! pos y new-y))))
 
   (add-method initialize
-      (make-method (list <pos>)
-	(lambda (call-next-method pos initargs)
-	  (move pos (getl initargs 'x 0) (getl initargs 'y 0)))))
+			  (make-method (list <pos>)
+						   (lambda (call-next-method pos initargs)
+							 (move pos (getl initargs 'x 0) (getl initargs 'y 0)))))
   )
 
 
@@ -126,17 +126,17 @@
 ;
 
 (define <class-slots-class>
-    (make-class (list <class>)
-		(list)))
+  (make-class (list <class>)
+			  (list)))
 
 (add-method compute-getter-and-setter
-    (make-method (list <class-slots-class>)
-      (lambda (call-next-method class slot allocator)
-	(if (null? (memq ':class-allocation slot))
-	    (call-next-method)
-	    (let ((cell '()))
-	      (list (lambda (o) cell)
-		    (lambda (o new) (set! cell new) new)))))))
+			(make-method (list <class-slots-class>)
+						 (lambda (call-next-method class slot allocator)
+						   (if (null? (memq ':class-allocation slot))
+							 (call-next-method)
+							 (let ((cell '()))
+							   (list (lambda (o) cell)
+									 (lambda (o new) (set! cell new) new)))))))
 
 
 ;
@@ -144,25 +144,25 @@
 ;
 ;
 (define <ship>
-    (make <class-slots-class>
-	  'direct-supers (list <object>)
-	  'direct-slots  (list 'name
-			       '(all-ships :class-allocation))))
+  (make <class-slots-class>
+		'direct-supers (list <object>)
+		'direct-slots  (list 'name
+							 '(all-ships :class-allocation))))
 
 (add-method initialize
-    (make-method (list <ship>)
-      (lambda (call-next-method ship initargs)
-	(call-next-method)
-	(initialize-slots ship initargs)
-	(slot-set! ship
-		   'all-ships
-		   (cons ship (slot-ref ship 'all-ships))))))
+			(make-method (list <ship>)
+						 (lambda (call-next-method ship initargs)
+						   (call-next-method)
+						   (initialize-slots ship initargs)
+						   (slot-set! ship
+									  'all-ships
+									  (cons ship (slot-ref ship 'all-ships))))))
 
 (define siblings (make-generic))
 (add-method siblings
-    (make-method (list <ship>)
-      (lambda (call-next-method ship)
-	(remove ship (slot-ref ship 'all-ships)))))
+			(make-method (list <ship>)
+						 (lambda (call-next-method ship)
+						   (remove ship (slot-ref ship 'all-ships)))))
 
 (define s1 (make <ship> 'name 's1))
 (define s2 (make <ship> 'name 's2))
@@ -180,60 +180,60 @@
 ;
 ;
 (define <dynamic-class>
-    (make-class (list <class>)
-		(list 'alist-g-n-s)))
+  (make-class (list <class>)
+			  (list 'alist-g-n-s)))
 
 
 (define dynamic-slot? (make-generic))
 
 (add-method dynamic-slot?
-    (make-method (list <dynamic-class>)
-      (lambda (call-next-method class slot)
-	(memq :dynamic-allocation (cdr slot)))))
+			(make-method (list <dynamic-class>)
+						 (lambda (call-next-method class slot)
+						   (memq :dynamic-allocation (cdr slot)))))
 
 
 
 (define alist-getter-and-setter
-    (lambda (dynamic-class allocator)
-      (let ((old (slot-ref dynamic-class 'alist-g-n-s)))
-	(if (null? old)
-	    (let ((new (allocator (lambda () '()))))
-	      (slot-set! dynamic-class 'alist-g-n-s new)
-	      new)
-	    old))))
+  (lambda (dynamic-class allocator)
+	(let ((old (slot-ref dynamic-class 'alist-g-n-s)))
+	  (if (null? old)
+		(let ((new (allocator (lambda () '()))))
+		  (slot-set! dynamic-class 'alist-g-n-s new)
+		  new)
+		old))))
 
 
 (add-method compute-getter-and-setter
-    (make-method (list <dynamic-class>)
-      (lambda (call-next-method class slot allocator)
-	(if (null? (dynamic-slot? class slot))
-	    (call-next-method)
-	    (let* ((name (car slot))
-		   (g-n-s (alist-getter-and-setter class allocator))
-		   (alist-getter (car g-n-s))
-		   (alist-setter (cadr g-n-s)))
-	      (list (lambda (o)
-		      (let ((entry (assq name  (alist-getter o))))
-			(if (null? entry)
-			    '()
-			    (cdr entry))))
-		    (lambda (o new)
-		      (let* ((alist (alist-getter o))
-                     (entry (assq name alist)))
-                (if (null? entry)
-                    (alist-setter o (cons (cons name new) alist))
-                    (set-cdr! entry new)) ;; !! R7RS plante ici !!
-			new))))))))
+			(make-method (list <dynamic-class>)
+						 (lambda (call-next-method class slot allocator)
+						   (if (null? (dynamic-slot? class slot))
+							 (call-next-method)
+							 (let* ((name (car slot))
+									(g-n-s (alist-getter-and-setter class allocator))
+									(alist-getter (car g-n-s))
+									(alist-setter (cadr g-n-s)))
+							   (list (lambda (o)
+									   (let ((entry (assq name  (alist-getter o))))
+										 (if (null? entry)
+										   '()
+										   (cdr entry))))
+									 (lambda (o new)
+									   (let* ((alist (alist-getter o))
+											  (entry (assq name alist)))
+										 (if (null? entry)
+										   (alist-setter o (cons (cons name new) alist))
+										   (set-cdr! entry new)) ;; !! R7RS plante ici !!
+										 new))))))))
 
 
 (define <all-dynamic-class>
-    (make-class (list <dynamic-class>)
-		(list)))
+  (make-class (list <dynamic-class>)
+			  (list)))
 
 (add-method dynamic-slot?
-    (make-method (list <all-dynamic-class>)
-      (lambda (call-next-method class slot) #t)))
-	    
+			(make-method (list <all-dynamic-class>)
+						 (lambda (call-next-method class slot) #t)))
+
 
 
 ;
@@ -241,13 +241,13 @@
 ;
 ;
 (define <person> (make <all-dynamic-class>
-		       'direct-supers (list <object>)
-		       'direct-slots  (list 'name 'age 'address)))
+					   'direct-supers (list <object>)
+					   'direct-slots  (list 'name 'age 'address)))
 
 (add-method initialize
-    (make-method (list <person>)
-      (lambda (call-next-method person initargs)
-	(initialize-slots person initargs))))
+			(make-method (list <person>)
+						 (lambda (call-next-method person initargs)
+						   (initialize-slots person initargs))))
 
 
 (define person1 (make <person> 'name 'sally))
@@ -263,28 +263,28 @@
 
 (define <db-class>
   (make-class (list <class>)
-	      (list 'id-g-n-s)))
+			  (list 'id-g-n-s)))
 
 (define id-getter-and-setter
-    (lambda (db-class allocator)
-      (let ((old (slot-ref db-class 'id-g-n-s)))
-	(if (null? old)
-	    (let ((new (allocator db-allocate-id)))
-	      (slot-set! class 'id-g-n-s new)
-	      new)
-	    old))))
+  (lambda (db-class allocator)
+	(let ((old (slot-ref db-class 'id-g-n-s)))
+	  (if (null? old)
+		(let ((new (allocator db-allocate-id)))
+		  (slot-set! class 'id-g-n-s new)
+		  new)
+		old))))
 
 (add-method compute-getter-and-setter
-    (make-method (list <db-class>)
-      (lambda (call-next-method class slot allocator)
-	(let* ((id-g-n-s (id-getter-and-setter class allocator))
-	       (id-getter (car id-g-n-s))
-	       (id-setter (cadr id-g-n-s))
-	       (slot-name (car slot)))
-	  (list (lambda (o)
-		  (db-lookup (id-getter o) slot-name)) 
-		(lambda (o new)
-		  (db-store  (id-getter o) slot-name new)))))))
+			(make-method (list <db-class>)
+						 (lambda (call-next-method class slot allocator)
+						   (let* ((id-g-n-s (id-getter-and-setter class allocator))
+								  (id-getter (car id-g-n-s))
+								  (id-setter (cadr id-g-n-s))
+								  (slot-name (car slot)))
+							 (list (lambda (o)
+									 (db-lookup (id-getter o) slot-name)) 
+								   (lambda (o new)
+									 (db-store  (id-getter o) slot-name new)))))))
 
 
 ;***
@@ -293,42 +293,42 @@
 ;
 ;
 (define make-around-generic
-    (lambda () (make <around-generic>)))
+  (lambda () (make <around-generic>)))
 
 (define make-around-method
-    (lambda (specializers procedure)
-      (make <around-method>
-	    'specializers specializers
-	    'procedure procedure)))
+  (lambda (specializers procedure)
+	(make <around-method>
+		  'specializers specializers
+		  'procedure procedure)))
 
 
 (define <around-generic> (make <entity-class>
-			       'direct-supers (list <generic>)))
+							   'direct-supers (list <generic>)))
 (define <around-method>  (make <class>
-			       'direct-supers (list <method>)))
+							   'direct-supers (list <method>)))
 
 
 (define around-method?   (make-generic))
 
 (add-method around-method?
-    (make-method (list <method>)
-      (lambda (call-next-method x) #f)))
+			(make-method (list <method>)
+						 (lambda (call-next-method x) #f)))
 (add-method around-method?
-    (make-method (list <around-method>)
-      (lambda (call-next-method x) #t)))
+			(make-method (list <around-method>)
+						 (lambda (call-next-method x) #t)))
 
 
 (add-method compute-methods
-    (make-method (list <around-generic>)
-      (lambda (call-next-method generic)
-	(let ((normal-compute-methods (call-next-method)))
-	  (lambda (args)
-	    (let ((normal-methods (normal-compute-methods args)))
-	      (append
-	        (filter-in around-method?
-			   normal-methods)
-		(filter-in (lambda (m) (not (around-method? m)))
-			   normal-methods))))))))
+			(make-method (list <around-generic>)
+						 (lambda (call-next-method generic)
+						   (let ((normal-compute-methods (call-next-method)))
+							 (lambda (args)
+							   (let ((normal-methods (normal-compute-methods args)))
+								 (append
+								   (filter-in around-method?
+											  normal-methods)
+								   (filter-in (lambda (m) (not (around-method? m)))
+											  normal-methods))))))))
 
 
 ;
@@ -341,20 +341,20 @@
 
 
 (define test-around
-    (lambda (generic)
-      (add-method generic
-	  (make-method        (list <foo>)
-	                      (lambda (cnm x) (cons 'foo (cnm)))))
+  (lambda (generic)
+	(add-method generic
+				(make-method        (list <foo>)
+									(lambda (cnm x) (cons 'foo (cnm)))))
 
-      (add-method generic
-	  (make-around-method (list <bar>)
-			      (lambda (cnm x) (cons 'bar (cnm)))))
+	(add-method generic
+				(make-around-method (list <bar>)
+									(lambda (cnm x) (cons 'bar (cnm)))))
 
-      (add-method generic
-	  (make-method        (list <baz>)
-	                      (lambda (cnm x) '(baz))))
+	(add-method generic
+				(make-method        (list <baz>)
+									(lambda (cnm x) '(baz))))
 
-      (generic (make <foo>))))
+	(generic (make <foo>))))
 
 
 (equal? (test-around (make-generic))        '(foo bar baz))
