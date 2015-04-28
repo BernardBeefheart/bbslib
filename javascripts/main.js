b5e0828a477edbd8c77e303dd65fce9a925bb9e5
bernard <bernard.beefheart@outlook.fr>
2015-04-29 00:50:18 +0200

    un CMS tout en JavaScript, super cool !

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,6 +1,23 @@
+$.urlParam = function(name, url) {
+    if (!url) {
+     url = window.location.href;
+    }
+    var results = new RegExp('[\\?&]' + name + '=([^&#]*)').exec(url);
+    if (!results) { 
+        return undefined;
+    }
+    return results[1] || undefined;
+}
+
 $(document).ready(function () {
+	var page = $.urlParam('page');
+	if (page == undefined) {
+		page = 'index';
+	}
+	page = '/bbslib/pages/bbslib/' + page + '.html';
 	$('#main_content').headsmart();
-	$( '#toc' ).load( 'http://bernardbeefheart.github.io/bbslib/content.html');
-	$( 'footer' ).load( 'http://bernardbeefheart.github.io/bbslib/footer.html');
+	$( '#toc' ).load( '/bbslib/pages/bbslib/content.html');
+	$( 'footer' ).load( '/bbslib/pages/bbslib/footer.html');
+	$( 'article' ).load( page);
 })
 
