51c5ec06978d69a162aa31899b1310a2876447d4
bernard <bernard.beefheart@outlook.fr>
2015-04-29 18:49:48 +0200

    j'ai un vrai CMS en javascript

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,23 +1,52 @@
 $.urlParam = function(name, url) {
-    if (!url) {
-     url = window.location.href;
-    }
-    var results = new RegExp('[\\?&]' + name + '=([^&#]*)').exec(url);
-    if (!results) { 
-        return undefined;
-    }
-    return results[1] || undefined;
+	if (!url) {
+		url = window.location.href;
+	}
+	var results = new RegExp('[\\?&]' + name + '=([^&#]*)').exec(url);
+	if (!results) { 
+		return 'index';
+	}
+	return results[1] || 'index';
 }
 
-$(document).ready(function () {
-	var page = $.urlParam('page');
-	if (page == undefined) {
-		page = 'index';
+function load_html(file_name, on_end_load) {
+	$.ajax(
+		{
+			url: file_name,
+			success: on_end_load
+		}
+	);
+}
+
+$(document).ready(
+	function () {
+		var page0 = $.urlParam('page');
+		var page = '/bbslib/pages/bbslib/' + page0 + '.html';
+		$('#main_content').headsmart();
+		load_html(page,
+				 function (result) {
+					 $('article').html(result);
+				 }
+	 );
+		load_html('/bbslib/pages/bbslib/content.html', 
+				  function(result){
+					  $('#toc').html(result);
+					  $('#toc ul li a').each(function( index, element ) {
+						  var lpage = $.urlParam('page', $( element ).attr('href'));
+						  if (lpage == page0) {
+							  var title = 'bbslib: ' + $(element).html();
+							  $('#main_title').html(title);
+							  document.title = title;
+							  $(element).addClass('italic');
+						  }
+					  })
+	  });
+		load_html('/bbslib/pages/bbslib/footer.html',
+				 function (result) {
+					 $('footer').html(result);
+				 }
+	 );
 	}
-	page = '/bbslib/pages/bbslib/' + page + '.html';
-	$('#main_content').headsmart();
-	$( '#toc' ).load( '/bbslib/pages/bbslib/content.html');
-	$( 'footer' ).load( '/bbslib/pages/bbslib/footer.html');
-	$( 'article' ).load( page);
-})
+);
+
 
