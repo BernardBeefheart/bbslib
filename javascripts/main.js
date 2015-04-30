1dca290a46411d8954917840b4ad4037987aec91
bernard <bernard.beefheart@outlook.fr>
2015-04-30 14:49:15 +0200

    légère amélioration du code JavaScript

--- javascripts/main.js
+++ javascripts/main.js
@@ -5,19 +5,20 @@ $.urlParam = function(name, url) {
 	var results = new RegExp('[\\?&]' + name + '=([^&#]*)').exec(url);
 	if (!results) { 
 		return 'index';
+	} else {
+		return results[1] || 'index';
 	}
-	return results[1] || 'index';
 }
 
-function load_page(page, on_end_load, place) {
+function load_page(page, place, on_end_load) {
 	var file_name = '/bbslib/pages/bbslib/' + page + '.html';
 	$.ajax(
 		{
 			url: file_name,
 			success: function(result) {
-				$(place).html(result);
+				$(place).html(result, place);
 				if (on_end_load) {
-					on_end_load(result);
+					on_end_load(result, place);
 				}
 			}
 		}
@@ -27,20 +28,21 @@ function load_page(page, on_end_load, place) {
 $(document).ready(
 	function () {
 		var page0 = $.urlParam('page');
-		load_page(page0, null, 'article');
-		load_page('content', 
-				  function(result){
-					  $('menu a').each(function( index, element ) {
+		load_page(page0, 'article');
+		load_page('content', 'menu', 
+				  function(result, place){
+					  $(place + ' a').each(function( index, element ) {
 						  var lpage = $.urlParam('page', $( element ).attr('href'));
 						  if (lpage == page0) {
 							  var title = 'bbslib: ' + $(element).html();
 							  $('#main_title').html(title);
 							  document.title = title;
 							  $(element).addClass('current-node');
+							  return false;
 						  }
 					  })
-		  }, 'menu');
-		load_page('footer', null, 'footer');
+		  });
+		load_page('footer', 'footer');
 	}
 );
 
